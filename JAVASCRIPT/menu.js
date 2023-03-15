26d074fa181db4ab8a7cbd28de8e6db6cef71128
[Noah van Ree] <noahvanree1201@gmail.com>
2023-03-15 11:25:24 +0100

    Menu

---
+++ JAVASCRIPT/menu.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+  
